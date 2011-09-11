e3c982ddbcdfe28a40e41a66e46f7f42f47c94db
Rich Jones <rich@anomos.info>
2011-09-11 18:36:11 -0400

    forgot to commit

--- ForkTube/forker/models.py
+++ ForkTube/forker/models.py
@@ -3,9 +3,12 @@ from django import forms
 from django.forms import ModelForm
 
 class ForkItem(models.Model):
-    user = forms.CharField(max_length=100)
-    password = forms.CharField(max_length=100)
-    url = forms.CharField(max_length=100)
+    user = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    url = models.CharField(max_length=100)
+    hashish = models.CharField(max_length=100)
+    uploaded = models.BooleanField(default=False)
+    downloaded = models.BooleanField(default=False)
 
 class ForkItemForm(ModelForm):
     class Meta:
